ae3f186f1f04a77ebed5e901b75bbd42896f7974
Felix Itzenplitz <felix@stkn.org>
2016-02-11 08:52:08 +0100

    update angular to beta6

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
 import {enableProdMode} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS} from 'angular2/router';
