a848675bc84eb5b74123b94c0aa1a517d80a0901
Felix Itzenplitz <felix@stkn.org>
2016-02-12 12:05:06 +0100

    update to angular beta7

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
 import {enableProdMode} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS} from 'angular2/router';
