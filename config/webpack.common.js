c610539ae35b6c61e958fb34c2bf1f366e494da6
Felix Itzenplitz <cebor@users.noreply.github.com>
2016-05-04 12:25:33 +0200

    Update webpack.common.js

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -14,6 +14,7 @@ module.exports = {
     ],
     vendor: [
       '@angular/common',
+      '@angular/compiler',
       '@angular/core',
       '@angular/http',
       '@angular/platform-browser-dynamic',
