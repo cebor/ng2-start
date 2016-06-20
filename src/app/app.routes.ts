b3db755d1d6deb68f091df9edab9abc9d27476e9
Felix Itzenplitz <felix@stkn.org>
2016-06-20 22:20:57 +0200

    fix router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,9 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 
 export const routes: RouterConfig = [
-  { path: '/', redirectTo: '/home', terminal: true },
-  { path: '/home', component: HomeComponent },
-  { path: '/about', component: AboutComponent },
+  { path: '', redirectTo: '/home', terminal: true },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
 ];
 
 export const APP_ROUTER_PROVIDERS = [
