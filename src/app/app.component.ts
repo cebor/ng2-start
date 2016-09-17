4248ca1b93b481aa25d760d9cc81b04c388f6470
Felix Itzenplitz <felix@stkn.org>
2016-09-17 21:20:23 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   ngOnInit() {
-      console.log('App loaded!!');
+    console.log('App loaded!!');
   }
 }
